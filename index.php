8103b56ac78195be8adbf44922ff358cc63c79cb
Laurenda Edorh <laurenda98@gmail.com>
2023-09-28 23:23:32 +0200

    integration d'autre fichier

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laurenda Edorh Portfolio</title>
+
+    <!--------Les liens------>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+
+</head>
+
+<body>
+    <!-------en tete-------->
+    <header>
+        <a href="#" class="logo">Cod<span>e</span>r.</a>
+
+        <ul class="navlist">
+            <li><a href="#home" class="active">Accueil</a></li>
+            <li><a href="#about">A propos de moi</a></li>
+            <li><a href="#services">Service</a></li>
+            <li><a href="#porfolio">Portfolio</a></li>
+            <li><a href="#contact">contact</a></li>
+        </ul>
+        <div class="bx bx-menu" id="menu-icon">
+
+        </div>
+
+    </header>
+    <!--------Fin en tete------->
+
+
+
+
+    <!------javacsript lien--------->
+    <script text="text/javascript" src="js/script.js"></script>
+
+</body>
+
+</html>
