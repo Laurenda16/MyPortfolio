f810b6c4a2122246f12e8545a2aa10ba5f5c7db4
Laurenda Edorh <laurenda98@gmail.com>
2023-09-29 05:02:26 +0200

    la partie service

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 
         <ul class="navlist">
             <li><a href="#home" class="active">Accueil</a></li>
-            <li><a href="#about">A propos de moi</a></li>
+            <li><a href="#about">Moi</a></li>
             <li><a href="#services">Service</a></li>
             <li><a href="#porfolio">Portfolio</a></li>
             <li><a href="#contact">contact</a></li>
@@ -59,16 +59,66 @@
         <div class="about-img">
             <img src="./image/image2.jpg" alt="">
         </div>
+        <div class="about-text">
+            <h2>About<span> Me</span></h2>
+            <h4>Developpeuse Web</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae tempora vitae, delectus voluptatibus eveniet id, maiores reprehenderit aperiam cum accusamus quam at modi illo non fuga, assumenda co tempore earum sit. Nesciunt sint animi fugit id unde corporis nisi, quis tenetur aut! Doloremque cum quis omnis quos, expedita sequi amet! Sapiente.
+                Rerum quia cum veritatis quis nostrum ducimus, nobis vel! Animi, dicta. Id molestiae quisquam unde, autem eaque odio quaerat voluptate eius nihil tenetur natus vel hic nobis totam similique quae?</p>
+            <a href="#" class="btn">Plus sur moi</a>
+        </div>
     </section>
 
 
 
     <!-------Fin apropos-------->
+    <!------Service-------->
+    <section class="services" id="services">
+        <div class="main-text">
+            <p>What I am Expert In</p>
+            <h2><span>Mes</span>Services</h2>
+        </div>
+
+        <div class="services-content">
+
+            <div class="box">
+                <div class="s-icons">
+                    <i class='bx bx-mobile-alt'></i>
+                </div>
+                <h2>Web Design</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic nostrum maxime laudantium quos sapiente quas officia odio autem quidem necessitatibus excepturi, asperiores voluptas voluptatibus facere nobis illum temporibus consequuntur praesentium?</p>
+                <a href="#" class=" read">Plus</a>
+            </div>
+            <div class="box">
+                <div class="s-icons">
+                    <i class='bx bx-devices'></i>
+                </div>
+                <h2>Realisation maquette</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic nostrum maxime laudantium quos sapiente quas officia odio autem quidem necessitatibus excepturi, asperiores voluptas voluptatibus facere nobis illum temporibus consequuntur praesentium?</p>
+                <a href="#" class=" read">Plus</a>
+            </div>
+            <div class="box">
+                <div class="s-icons">
+                    <i class='bx bx-code-alt'></i>
+                </div>
+                <h2>Conception Site Web</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic nostrum maxime laudantium quos sapiente quas officia odio autem quidem necessitatibus excepturi, asperiores voluptas voluptatibus facere nobis illum temporibus consequuntur praesentium?</p>
+                <a href="#" class=" read">Plus</a>
+            </div>
+
+
+        </div>
+    </section>
+    <!------Fib Service-------->
 
 
 
     <!------javacsript lien--------->
-    <script text="text/javascript" src="js/script.js"></script>
+
+    <script type="text/javascript" src="/js/script.js">
+
+    </script>
+
+    </script>
 
 </body>
 
