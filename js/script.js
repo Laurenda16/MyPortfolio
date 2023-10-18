c1350d60979c0bc8461ed89b4af3447655e47afd
Laurenda Edorh <laurenda98@gmail.com>
2023-10-18 22:39:45 +0200

    ajustement des elements de basse

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 const entete = document.querySelector("header");
 
-window.addEventListener("scroll", function   () {
+window.addEventListener("scroll", function () {
   entete.classList.toggle("sticky", window.scrollY > 100);
 });
-   
+
+let menu = document.querySelector("#icon");
+let navlist = document.querySelector(".navlist");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
+};
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navlist.classList.remove("open");
+};
